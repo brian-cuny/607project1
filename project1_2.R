03ec9bd7ca98c012fc5a92421c87b0ca6ebf272e
Brian W <brian.weinfeld+cuny@gmail.com>
2018-02-18 10:51:36 -0500

    rough draft

--- project1_2.R
+++ project1_2.R
@@ -6,36 +6,35 @@ raw.data <- read.fwf(con, widths=c(6, 2, 32, 1, 5, 43), skip=4, stringsAsFactors
 close(con)
   
 players.row.1 <- raw.data[seq(1, nrow(raw.data), 3), ] %>% 
-  setNames(c('number', 'd1', 'name', 'd2', 'points', 'post')) %>% 
+  setNames(c('number', 'd1', 'name', 'd2', 'points', 'opponents')) %>% 
   select(-d1, -d2)
 players.row.2 <- raw.data[seq(2, nrow(raw.data), 3), ] %>% 
   setNames(c('location', 'd1', 'pre', 'd2', 'd3', 'd4')) %>% 
   select(-d1, -d2, -d3, -d4) %>% 
   mutate(number = players.row.1$number)
 
-players.combined <- left_join(players.row.1, players.row.2, by='number')
+players.combined <- left_join(players.row.1, players.row.2, by='number') %>% 
+  subset(select=c(2, 5, 3, 6, 4))
 
 players.combined$pre <- players.combined$pre %>% 
   str_sub(15, 18) %>% 
   as.numeric()
-players.combined$post <- players.combined$post %>% 
+players.combined$opponents <- players.combined$opponents %>% 
   lapply(. %>% 
-           str_split('\\|') %>% 
+           str_extract_all('\\d+') %>% 
            unlist() %>% 
-           parse_number() %>% 
-           na.omit() %>% 
            as.numeric() %>% 
            mapvalues(1:nrow(players.combined), players.combined$pre) %>% 
            mean() %>% 
            round(digits=0)
-         )
+  )
+  
 
-numeric.cols <- c('number', 'points', 'games')
-character.cols <- c('name', 'location')
-
-players.combined[, numeric.cols] <- players.combined[, numeric.cols] %>% 
+players.combined[, 3:5] <- players.combined[, 3:5] %>% 
   sapply(., as.numeric) 
-players.combined[, character.cols] <- players.combined[, character.cols] %>% 
+players.combined[, 1:2] <- players.combined[, 1:2] %>% 
   sapply(., str_trim) 
+write.csv(players.combined, file='ChessData.csv')
+
 
 
