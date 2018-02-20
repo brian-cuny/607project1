ad03b5b2e22df3ee3be32f61e23cd367d84d80e4
Brian W <brian.weinfeld+cuny@gmail.com>
2018-02-20 16:28:50 -0500

    included map functions

--- project1_2.R
+++ project1_2.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(plyr)
+library(magrittr)
 
 con <- file('C:\\Users\\Brian\\Desktop\\GradClasses\\Spring18\\607\\projects\\tournamentinfo.txt', open='r')
 raw.data <- read.fwf(con, widths=c(6, 2, 32, 1, 5, 43), skip=4, stringsAsFactors=FALSE) 
@@ -11,29 +12,27 @@ players.row.1 <- raw.data[seq(1, nrow(raw.data), 3), ] %>%
 players.row.2 <- raw.data[seq(2, nrow(raw.data), 3), ] %>% 
   setNames(c('location', 'd1', 'pre', 'd2', 'd3', 'd4')) %>% 
   select(-d1, -d2, -d3, -d4) %>% 
-  mutate(number = players.row.1$number)
+  mutate(number=players.row.1$number)
 
 players.combined <- left_join(players.row.1, players.row.2, by='number') %>% 
-  subset(select=c(2, 5, 3, 6, 4))
+  subset(select=c(2, 5, 3, 6, 4)) %>%
+  map_df(str_trim)
 
-players.combined$pre <- players.combined$pre %>% 
+
+players.combined$points %<>% 
+  as.numeric()
+players.combined$pre %<>% 
   str_sub(15, 18) %>% 
   as.numeric()
-players.combined$opponents <- players.combined$opponents %>% 
-  lapply(. %>% 
-           str_extract_all('\\d+') %>% 
-           unlist() %>% 
-           as.numeric() %>% 
-           mapvalues(1:nrow(players.combined), players.combined$pre) %>% 
-           mean() %>% 
-           round(digits=0)
-  )
-  
+players.combined$opponents %<>% 
+  map_dbl(. %>% str_extract_all('\\d+') %>%
+            unlist() %>% 
+            as.numeric() %>% 
+            mapvalues(seq_along(players.combined$pre), players.combined$pre) %>% 
+            mean() %>% 
+            round()
+         )
 
-players.combined[, 3:5] <- players.combined[, 3:5] %>% 
-  sapply(., as.numeric) 
-players.combined[, 1:2] <- players.combined[, 1:2] %>% 
-  sapply(., str_trim) 
 write.csv(players.combined, file='ChessData.csv')
 
 
